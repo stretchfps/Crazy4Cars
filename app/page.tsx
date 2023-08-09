cb367a216dd198dea822eb9bf71eda3eeeb1a7f7
Shubham Mehra <135194469+stretchfps@users.noreply.github.com>
2023-08-09 15:58:45 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import { fetchCars } from "@utils";
-import { HomeProps } from "@types";
-import { fuels, yearsOfProduction } from "@constants";
-import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@components";
+import Image from 'next/image'
+
+import { CarCard, Hero, ShowMore } from '@/components'
+import SearchBar from '@/components/SearchBar'
+import CustomFilter from '@/components/CustomFilter'
+import { fetchCars } from '@/utils'
+import { fuels, yearsOfProduction } from '@/constants'
 
 export default async function Home({ searchParams }: HomeProps) {
   const allCars = await fetchCars({
